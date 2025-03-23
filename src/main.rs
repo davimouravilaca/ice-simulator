f7348fbcd9ce19038c8ab7fdad968dd6a733a064
davimouravilaca <davimouravilaca@gmail.com>
2025-03-23 18:51:03 -0300

    engine struct

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,20 @@ mod modules {
     pub mod engine;
 }
 
+use crate::modules::engine::{Engine, Disposition};
+
 fn main() {
-    println!("Hello, world!");
+
+    let cylinders: i8 = 8;
+    let disposition = Disposition::V;
+    let name = "HEMI V8".to_string();
+
+
+    let engine = Engine::new(
+        cylinders,
+        disposition,
+        name
+    );
+
+    println!("Engine created: {} cylinders, disposition: {:?}, name: {}", engine.cylinders, engine.disposition, engine.name);
 }
--- src/modules/engine.rs
+++ src/modules/engine.rs
@@ -1,3 +1,23 @@
-mod engine {
-    
-}
+    #[derive(Debug)]
+    pub enum Disposition {
+        Inline,
+        V,
+        Boxer,
+    }
+
+    pub struct Engine {
+        pub cylinders: i8,
+        pub disposition: Disposition,
+        pub name: String
+    }
+
+    impl Engine {
+        pub fn new(cylinders: i8, disposition: Disposition, name: String) -> Self {
+            Self {
+                cylinders,
+                disposition,
+                name
+            }
+
+        }
+    }
