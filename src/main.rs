f1460bd6379c7365899e157f5733ed68eaa1c171
davimouravilaca <davimouravilaca@gmail.com>
2025-03-23 19:08:26 -0300

    cylinder diameter and piston stroke params

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,27 @@ use crate::modules::engine::{Engine, Disposition};
 fn main() {
 
     let cylinders: i8 = 8;
+    let cylinder_diameter: f32 = 99.5;
+    let cylinder_stroke: f32 = 90.9;
     let disposition = Disposition::V;
     let name = "HEMI V8".to_string();
 
 
     let engine = Engine::new(
         cylinders,
+        cylinder_diameter,
+        cylinder_stroke,
         disposition,
         name
     );
 
-    println!("Engine created: {} cylinders, disposition: {:?}, name: {}", engine.cylinders, engine.disposition, engine.name);
+    println!(
+        "Engine created: {} cylinders, cylinder diameter: {}, piston stroke: {}, disposition: {:?}, name: {:?}",
+        engine.cylinders,
+        engine.cylinder_diameter,
+        engine.cylinder_stroke,
+        engine.disposition,
+        engine.name
+    );
+
 }
--- src/modules/engine.rs
+++ src/modules/engine.rs
@@ -7,14 +7,18 @@
 
     pub struct Engine {
         pub cylinders: i8,
+        pub cylinder_diameter: f32,
+        pub cylinder_stroke: f32,
         pub disposition: Disposition,
         pub name: String
     }
 
     impl Engine {
-        pub fn new(cylinders: i8, disposition: Disposition, name: String) -> Self {
+        pub fn new(cylinders: i8, cylinder_diameter: f32, cylinder_stroke: f32, disposition: Disposition, name: String) -> Self {
             Self {
                 cylinders,
+                cylinder_diameter,
+                cylinder_stroke,
                 disposition,
                 name
             }
