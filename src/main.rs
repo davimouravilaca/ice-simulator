7303741b15478c9104bd7f95d135cb519c3ffa6d
davimouravilaca <davimouravilaca@gmail.com>
2025-03-25 09:13:29 -0300

    engine displacement calc

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,30 @@ use crate::modules::engine::{Engine, Disposition};
 
 fn main() {
 
-    let cylinders: i8 = 8;
+    let cylinders: u8 = 8;
     let cylinder_diameter: f32 = 99.5;
     let cylinder_stroke: f32 = 90.9;
     let disposition = Disposition::V;
     let name = "HEMI V8".to_string();
-
+    let engine_displacement = Engine::engine_displacement(cylinders, cylinder_diameter, cylinder_stroke);
 
     let engine = Engine::new(
         cylinders,
         cylinder_diameter,
         cylinder_stroke,
         disposition,
+        engine_displacement,
         name
     );
 
     println!(
-        "Engine created: {} cylinders, cylinder diameter: {}, piston stroke: {}, disposition: {:?}, name: {:?}",
+        "Engine created: {} cylinders, cylinder diameter: {}, piston stroke: {}, disposition: {:?}, name: {:?}, engine displacement: {} L",
         engine.cylinders,
         engine.cylinder_diameter,
         engine.cylinder_stroke,
         engine.disposition,
-        engine.name
+        engine.name,
+        engine.engine_displacement
     );
 
 }
--- src/modules/engine.rs
+++ src/modules/engine.rs
@@ -1,3 +1,11 @@
+    const PI :f32 = 3.1416;
+
+    pub fn cm_to_lt(num: f32) -> f32 {
+        let num = (num / 100000.0).ceil() * 0.1;
+        (num * 10.0).trunc() / 10.0
+    }
+
+    
     #[derive(Debug)]
     pub enum Disposition {
         Inline,
@@ -6,22 +14,30 @@
     }
 
     pub struct Engine {
-        pub cylinders: i8,
+        pub cylinders: u8,
         pub cylinder_diameter: f32,
         pub cylinder_stroke: f32,
         pub disposition: Disposition,
+        pub engine_displacement: f32,
         pub name: String
     }
 
     impl Engine {
-        pub fn new(cylinders: i8, cylinder_diameter: f32, cylinder_stroke: f32, disposition: Disposition, name: String) -> Self {
+        pub fn new(cylinders: u8, cylinder_diameter: f32, cylinder_stroke: f32, disposition: Disposition, engine_displacement: f32, name: String) -> Self {
             Self {
                 cylinders,
                 cylinder_diameter,
                 cylinder_stroke,
                 disposition,
+                engine_displacement,
                 name
             }
+        }
+        
+        pub fn engine_displacement(cylinders: u8, cylinder_diameter: f32, cylinder_stroke: f32) -> f32 {
 
+            let cylinders: f32 = cylinders as f32;
+            let engine_displacement = (PI / 4 as f32) * (cylinder_diameter * cylinder_diameter) * cylinder_stroke * cylinders;
+            return cm_to_lt(engine_displacement);
         }
     }
